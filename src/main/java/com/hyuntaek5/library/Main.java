739f9489324498aa62e3a2f639c9cc364bc10b1c
HyunTaek Oh <oht222@naver.com>
2021-05-03 01:55:10 +0900

    Complete Quit method

--- src/main/java/com/hyuntaek5/library/Main.java
+++ src/main/java/com/hyuntaek5/library/Main.java
@@ -13,14 +13,19 @@ public class Main {
         String ch1;
         int ch2;
 
+        System.out.println("Welcome to Library Manage System.\n");
         while (true) {
             if(user.loginMember()==null){
                 do{
-                    System.out.print("[1]Login\n[2]SignIn\n[3]Quit\n");
+                    System.out.print("[1] Login\n[2] SignIn\n[3] Quit(To Quit, Press Q)\n");
                     ch1 = sc.next();
+                    try {
                     ch2 = Integer.parseInt(ch1);
-                } while (ch2<1||ch2>2);
-                if (ch1.equals("Q")) break;
+                    } catch (NumberFormatException e){
+                       ch2 = 3;
+                    }
+                } while (ch2<1||ch2>3);
+                if (ch2==3||ch1.equals("Q")) break;
                 switch (ch2){
                     case 1:{
                         user.login();
@@ -33,7 +38,7 @@ public class Main {
                 }
             } else if(user.loginMember().getId().equals("admin")) {
                 do {
-                    System.out.print("[1]Manage Book\n[2]LogOut\n[3]Return\n");
+                    System.out.print("[1] Manage Book\n[2] LogOut\n[3] Return\n");
                     ch2 = sc.nextInt();
                 } while (ch2<1||ch2>3);
 
@@ -41,7 +46,7 @@ public class Main {
 
                 switch (ch2) {
                     case 1: do{
-                        System.out.println("[1]Add Book\n[2]Delete Book\n[3]BookList\n[4]Return\n");
+                        System.out.println("[1] Add Book\n[2] Delete Book\n[3] BookList\n[4] Return\n");
                         ch2 = sc.nextInt();
                     } while (ch2<1||ch2>4);
                     if(ch2==4) break;
@@ -57,14 +62,16 @@ public class Main {
             }
             else {
                 do{
-                    System.out.print("[1]Borrow Book\n[2]Back Book\n[3]Look at BookList\n[4]LogOut\n[5]Delete User\n[6]Quit(Input Q)\n");
-                    ch2 = sc.nextInt();
+                    System.out.print("[1] Borrow Book\n[2] Back Book\n[3] Look at BookList\n[4] LogOut\n" +
+                            "[5] Delete User\n[6] Quit(To Quit, Press Q)\n");
+                    ch1 = sc.next();
+                    try {
+                        ch2 = Integer.parseInt(ch1);
+                    } catch (NumberFormatException e){
+                        ch2 = 6;
+                    }
                 } while (ch2<1||ch2>6);
-                if(ch2 == 6){
-                    System.out.println("If you want to Quit, Press Q.\n");
-                    ch1 = sc.nextLine();
-                    if (ch1.equals("Q")) break;
-                }
+                if(ch2 == 6||ch1.equals("Q")) break;
                 switch (ch2) {
                     case 1: book.borrow(); break;
                     case 2: book.back(); break;
